89ccadb34f4137aa2c065b84fad0d4a0ea13c34c
Rafael Carlos <shanglangjunior@hotmail.com>
2017-09-28 17:03:12 -0300

    Inicializando repositório

--- app/src/main/java/com/codigo/rafael/easygas/MainActivity.java
+++ app/src/main/java/com/codigo/rafael/easygas/MainActivity.java
@@ -1,13 +1,30 @@
 package com.codigo.rafael.easygas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bt_teste)
+    Button btTeste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btTeste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ActivityTeste.class));
+            }
+        });
     }
 }
